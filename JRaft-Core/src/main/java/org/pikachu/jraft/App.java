4233f12284a568b89f6c8d3f99eb7be8b07174ab
picaqiu <673396844@qq.com>
2024-01-09 18:11:34 +0800

    feature/create java raft project

---
+++ JRaft-Core/src/main/java/org/pikachu/jraft/App.java
@@ -0,0 +1,13 @@
+package org.pikachu.jraft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JRaft-Core/src/main/java/org/pikachu/jraft/enums/Role.java
@@ -0,0 +1,10 @@
+package org.pikachu.jraft.enums;
+
+public enum Role {
+    FOLLOWER,
+
+    LEADER,
+
+    CANDIDATE
+
+}
